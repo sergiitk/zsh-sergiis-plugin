f75c6105592d5ae3f058e312bba7b5ff63d38843
Sergii Tkachenko <hi@sergii.org>
2023-09-28 20:22:05 -0700

    get-power

--- osx/functions.zsh
+++ osx/functions.zsh
@@ -118,3 +118,15 @@ get-adapter-power() {
    plutil -extract '0.BatteryData.AdapterPower' raw - |\
    python -c 'import sys, struct; print(format(struct.unpack(">f", struct.pack(">l", int(sys.stdin.read())))[0], ".2f"), "W")'
 }
+
+get-system-power() {
+  ioreg -rw0 -a -c AppleSmartBattery |\
+   plutil -extract '0.BatteryData.SystemPower' raw - |\
+   python -c 'import sys, struct; print(format(struct.unpack(">f", struct.pack(">l", int(sys.stdin.read())))[0], ".2f"), "W")'
+}
+
+
+get-power() {
+  echo "Using:    $(get-system-power)"
+  echo "Charging: $(get-adapter-power)"
+}
