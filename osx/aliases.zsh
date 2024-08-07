ea54a82e95f0d14e5053a95c05067f68fad2b2c0
Sergii Tkachenko <sergiitk@google.com>
2024-08-07 14:55:50 -0700

    update pup alias

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -13,8 +13,27 @@ alias f='open "$(pwd)"'
 alias o="open"
 
 # Port.
-alias pup='sudo port selfupdate && port echo outdated && sudo port upgrade outdated'
-alias pca='sudo port -f clean --all all'
+# alias pup='sudo port selfupdate \
+#   && echo -e "\nRequested and outdated:" \
+#   && port echo requested and outdated \
+#   && echo -e "\nOutdated:" \
+#   && port outdated \
+#   && sudo port upgrade outdated'
+unalias pup
+pup() {
+  sudo port selfupdate
+  if (( $? != 0 )); then
+      return
+  fi
+  echo
+  echo "Outdated:"
+  port outdated
+  echo
+  port echo "Requested and outdated:" requested and outdated
+  echo
+  sudo port upgrade outdated
+}
+# alias pca='sudo port -f clean --all all'
 
 # Utils.
 alias df/='gdf --si --output=source,used,pcent / | tail -1 | column -t'
