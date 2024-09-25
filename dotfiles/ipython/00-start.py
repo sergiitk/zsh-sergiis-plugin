deaae2155e40ff3d3527ce64b511181dd6ece29f
Sergii Tkachenko <sergiitk@google.com>
2024-09-25 11:15:06 -0700

    add py rgb method

--- dotfiles/ipython/00-start.py
+++ dotfiles/ipython/00-start.py
@@ -1,9 +1,14 @@
 import functools
 import datetime as dt
 
+
 def time_diff(s, e):
     ds = dt.datetime.combine(dt.date.today(), dt.time.fromisoformat(s))
     de = dt.datetime.combine(dt.date.today(), dt.time.fromisoformat(e))
     d = de - ds
     print(f'Delta {d} (h:mm:ss)')
     return d
+
+
+def rgb(r, g, b):
+  print(hex(r << 16 | g << 8 | b).replace("0x", "#", 1))
