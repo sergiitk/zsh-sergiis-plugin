e185f5ac6a1eb3610c000f9a89f8c902a0ab47f2
Sergii Tkachenko <sergiitk@google.com>
2025-03-07 15:34:52 -0800

    precisedelta helpers

--- dotfiles/ipython/00-start.py
+++ dotfiles/ipython/00-start.py
@@ -3,14 +3,17 @@ import datetime as dt
 
 try:
   import humanize
+  from humanize import precisedelta as pdelta
 except ModuleNotFoundError:
-  pass
+  def pdelta(delta, *args, **kwargs):
+    return f'Delta {delta} (h:mm:ss)'
+
 
 def time_diff(s, e):
     ds = dt.datetime.combine(dt.date.today(), dt.time.fromisoformat(s))
     de = dt.datetime.combine(dt.date.today(), dt.time.fromisoformat(e))
     d = de - ds
-    print(f'Delta {d} (h:mm:ss)')
+    print(pdelta(d))
     return d
 
 
