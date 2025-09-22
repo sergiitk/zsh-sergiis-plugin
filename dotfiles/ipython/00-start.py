f38542c5d5896b280cc98e4aa8b601813d2bc27a
Sergii Tkachenko <sergiitk@google.com>
2025-09-22 10:02:40 -0700

    ipython human_sec

--- dotfiles/ipython/00-start.py
+++ dotfiles/ipython/00-start.py
@@ -2,11 +2,12 @@ import functools
 import datetime as dt
 
 try:
-  import humanize
-  from humanize import precisedelta as pdelta
+    import humanize
+    from humanize import precisedelta as pdelta
 except ModuleNotFoundError:
-  def pdelta(delta, *args, **kwargs):
-    return f'Delta {delta} (h:mm:ss)'
+
+    def pdelta(delta, *args, **kwargs):
+        return f"Delta {delta} (h:mm:ss)"
 
 
 def time_diff(s, e):
@@ -17,5 +18,18 @@ def time_diff(s, e):
     return d
 
 
+def human_sec(sec: float | str, mu: str = "seconds"):
+
+    if isinstance(sec, str):
+        sec_str = sec.rstrip().removesuffix("sec").removesuffix("s").rstrip()
+        sec_float = float(sec_str)
+    else:
+        sec_float = sec
+
+    delta = dt.timedelta(seconds=sec_float)
+    result = pdelta(delta, minimum_unit=mu)
+    return result
+
+
 def rgb(r, g, b):
-  print(hex(r << 16 | g << 8 | b).replace("0x", "#", 1))
+    print(hex(r << 16 | g << 8 | b).replace("0x", "#", 1))
