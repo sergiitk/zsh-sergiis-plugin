1b7019938d91df43af9b85f28f12bb3b0fd00187
Sergii Tkachenko <sergiitk@google.com>
2025-09-21 20:38:42 -0700

    same for home + upd ssh-load-keychain

--- dotfiles/home/.p10k.zsh
+++ dotfiles/home/.p10k.zsh
@@ -461,7 +461,10 @@
 
     # Display "wip" if the latest commit's summary contains "wip" or "WIP".
     if [[ $VCS_STATUS_COMMIT_SUMMARY == (|*[^[:alnum:]])(wip|WIP)(|[^[:alnum:]]*) ]]; then
-      res+=" ${modified}wip"
+      # res+=" ${modified}wip"
+
+      # my mod: wip in square brackets
+      res+=" ${modified}[wip]"
     fi
 
     if (( VCS_STATUS_COMMITS_AHEAD || VCS_STATUS_COMMITS_BEHIND )); then
