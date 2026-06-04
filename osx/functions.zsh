936b8949de35c2f51e39e65a0fc723774ca8d9f6
Sergii Tkachenko <sergiitk@google.com>
2026-06-03 22:57:58 -0700

    switch-resolution code style

--- osx/functions.zsh
+++ osx/functions.zsh
@@ -70,17 +70,17 @@ switch-resolution() {
   local display_info current_mode_str current_mode_int
 
   display_info="$(displayplacer list | sed -n "/${serial}/,/^$/p" | grep -v "color_depth:4" | head -n-1)"
-  if [[ -z $display_info ]]; then
+  if [[ -z "${display_info}" ]]; then
     print -u2 -- "Serial number not found: ${serial}"
     return 1
   fi
-  if [[ $set_mode == "-l" ]]; then
+  if [[ "${set_mode}" == "-l" ]]; then
     echo "${display_info}"
     return 0
   fi
 
-  current_mode_str="$(echo "$display_info" | grep -oE 'mode [0-9]+: .*<-- current mode')"
-  current_mode_int="$(echo "$current_mode_str" | grep -oP '(?<=mode )[0-9]+(?=:)')"
+  current_mode_str="$(echo "${display_info}" | grep -oE 'mode [0-9]+: .*<-- current mode')"
+  current_mode_int="$(echo "${current_mode_str}" | grep -oP '(?<=mode )[0-9]+(?=:)')"
   if [[ -z "${current_mode_str}" || -z "${current_mode_int}" ]]; then
     print -u2 -- "Current mode not detected"
     return 1
@@ -94,14 +94,14 @@ switch-resolution() {
     else
       set_mode="${normal_mode}"
     fi
-  elif [[ $set_mode != [[:digit:]]## ]]; then
+  elif [[ "${set_mode}" != [[:digit:]]## ]]; then
     print -u2 -- "Wrong mode format: ${set_mode}"
     return 1
   fi
 
   local new_mode_str
-  new_mode_str=$(echo "$display_info" | grep -oE "mode ${set_mode}: .*")
-  if [[ -z $new_mode_str ]]; then
+  new_mode_str=$(echo "${display_info}" | grep -oE "mode ${set_mode}: .*")
+  if [[ -z "${new_mode_str}" ]]; then
     print -u2 -- "Requested mode not found: ${set_mode}"
     return 1
   fi
